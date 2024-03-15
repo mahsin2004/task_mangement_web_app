eb7d791d786262f95afb90897ed482a7cf55c4bd
mahsin <mahsin1912@gmail.com>
2024-03-15 15:56:18 +0600

    Reday for development

---
+++ src/home/Home.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+    return (
+        <div>
+            <Button>Hello EveryOne</Button>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+      ],
+  },
+]);
+
+export default router;
